3e0284feb2c4b4e56103e18a5691298d88d66054
Emil O'Brien <ejobrien714@gmail.com>
2016-04-12 12:27:59 -0400

    added crud for roles

---
+++ ReviewerFinal/Controllers/IdentityRolesController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ReviewerFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReviewerFinal.Controllers
+{
+    public class IdentityRolesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: IdentityRoles
+        public ActionResult Index()
+        {
+            return View(db.Roles.ToList());
+        }
+
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            IdentityRole role = db.Roles.Find(id);
+
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(role);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include ="ID,Name")] IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Roles.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(role);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            IdentityRole role = db.Roles.Find(id);
+
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(role);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name")] IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(role);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            IdentityRole role = db.Roles.Find(id);
+
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(role);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            IdentityRole temp = db.Roles.Find(id);
+            db.Roles.Remove(temp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

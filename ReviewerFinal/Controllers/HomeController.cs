a6516608be15212024e19e6d4dd9dac0f9b62a3f
Emil O'Brien <ejobrien714@gmail.com>
2016-04-23 16:13:36 -0400

    added carousel

--- ReviewerFinal/Controllers/HomeController.cs
+++ ReviewerFinal/Controllers/HomeController.cs
@@ -1,27 +1,36 @@
-﻿using ReviewerFinal.CustomAttribute;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
+using ReviewerFinal.Models;
+using ReviewerFinal.CustomAttribute;
 
 namespace ReviewerFinal.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [AllowAnonymous]
         public ActionResult Index()
         {
+            var games = db.Games.ToList();
+            var listOfPics = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                listOfPics.Add( games[i].GameImage);
+            }
+
+            ViewBag.FirstFiveGames = listOfPics;
+
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
+        [AllowAnonymous]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
